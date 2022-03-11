6f6681c6e66023fbbd4c62e8233fc9769dd4a8e9
Rinakae <simi1993@mail.ru>
2022-03-11 23:58:51 +0300

    chenge script => debounce

--- script.js
+++ script.js
@@ -1,68 +1,19 @@
-const getDate = () => {
-  const goodDay = document.getElementById("good-day");
-  const todayDay = document.getElementById("today-day");
-  const todayTime = document.getElementById("today-time");
-  const newYear = document.getElementById("new-year");
+const inputDb = document.getElementById("inputDb");
+const pDb = document.getElementById("pDb");
 
-  let timerID = null;
-  let timerIDYear = null;
+function onInput(event) {
+  pDb.textContent = event.target.value;
+}
 
-  const getDateValue = () => {
+let debounceOnInput = debounce(onInput);
 
-    let h = new Date().getHours();
-    
-    if (h <= 6) {
-      goodDay.textContent = "Доброй ночи";
-    } else if (h <= 11) {
-      goodDay.textContent = "Доброе утро";
-    } else if (h <= 18) {
-      goodDay.textContent = "Добрый день";
-    } else {
-      goodDay.textContent = "Добрый вечер";
-    }
+inputDb.addEventListener("input", debounceOnInput);
 
+function debounce(callback) {
+  let timeout;
+
+  return function(argument) {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, 300, argument);
   };
-
-  const getDayToday = () => {
-    const week = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
-
-    let day = new Date().getDay();
-    let dayToday = week[day];
-    
-    todayDay.textContent = `Сегодня: ${dayToday}`;
-  };
-
-  const getTimeNow = () => {
-    let date = new Date();
-        
-    let options = {       
-      timezone: "UTC",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric"
-    };
-
-    let clock = date.toLocaleString("en-US", options);
-    
-    todayTime.textContent = `Текущее время: ${clock}`;
-  };
-
-  const getNewYear = () => {
-    let dateStop = new Date("1 january 2023").getTime();
-    let dateNow = new Date().getTime();
-    let timeRemaining = (dateStop - dateNow) / 1000;
-    let newYDays = Math.floor((timeRemaining / 60 / 60) / 24);
-
-    newYear.textContent = `До нового года осталось ${newYDays} дней`;
-  };
-
-  getDateValue();
-  getDayToday();
-  getTimeNow();
-  timerID = setInterval(getTimeNow, 1000);
-  getNewYear();
-  timerIDYear = setInterval(getNewYear, 86400 * 1000);
-
-};
-
-getDate();
+}
