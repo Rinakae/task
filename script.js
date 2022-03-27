679160fd4be26e392ac6f7dba28f8e0243072c4f
Rinakae <simi1993@mail.ru>
2022-03-27 22:40:51 +0300

    promise and fetch

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const getData = () => {
+  fetch("db.json")  
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      sendData(data);
+    });
+};
+
+const sendData = (data) => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+getData();
