e79ea57c99b7ec66edfae97fbf04ac39962cb5da
Rinakae <simi1993@mail.ru>
2022-03-28 20:35:42 +0300

    move div block keydown

--- script.js
+++ script.js
@@ -32,32 +32,45 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   };
 };
 
-DomElement.prototype.move = function() {
-  
-  console.log(this.position);
-  
-};
-
 let DomElement2 = new DomElement('#divdiv', '50px', '300px', 'tomato', '24px');
 DomElement2.createElement();
 
-/*
+
 let square = new DomElement('.squaer', '100px', '100px', 'pink', '');
-square.position = 'absolute';
-square.element = () => {
-  this.element = document.createElement('p');
-      this.element.className = this.selector;
-      this.element.textContent = "абзац p";
-      this.element.style.cssText = `height: ${this.height}; 
-                         width: ${this.width};
-                         background-color: ${this.bg};
-                         font-size: ${this.fontSize};`;
-      document.body.append(this.element);  
-};
 square.createElement();
-square.move();
-console.log(square);
-*/
+let squaerElement = square.element;
+
+squaerElement.style.position = "absolute";
+
+console.log(square.element);
+
+document.addEventListener( "keydown", (e) => {
+
+    let  cs = window.getComputedStyle(squaerElement);     
+    let  left = parseInt(cs.marginLeft);
+    let  top = parseInt(cs.marginTop);
+     
+    switch(e.key){         
+      case "ArrowLeft":  
+        if (left > 0)
+          { squaerElement.style.marginLeft = left - 10 + "px"; }
+        break;
+      case "ArrowUp": 
+        if (top > 0)
+          { squaerElement.style.marginTop = top - 10 + "px"; }
+        break;
+      case "ArrowRight":   
+        if (left < document.documentElement.clientWidth - 100)
+          { squaerElement.style.marginLeft = left + 10 + "px"; }
+        break;
+      case "ArrowDown":  
+        if (top < document.documentElement.clientHeight - 100)
+          { squaerElement.style.marginTop = top + 10 + "px"; }
+        break;
+    }
+  
+});
+
 
 
 
